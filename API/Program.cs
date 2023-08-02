fa8cc742adeac9cb33fe158b322fe1c54478d8cc
Eleanor Sisley <cion.art.design@gmail.com>
2023-08-02 09:02:21 +0100

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,8 +13,10 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,6 +24,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(opt => 
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
+
 
 app.UseAuthorization();
 
---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,38 @@
+import { ThemeProvider, createTheme, CssBaseline, Container } from "@mui/material";
+import { useState } from "react";
+import Catalog from "../../features/catalog/Catalog";
+import Header from "./Header";
+
+
+function App() {
+const [darkMode, setDarkMode] = useState(false);
+const paletteType = darkMode ? 'dark' : 'light';
+ const theme = createTheme({
+  palette: {
+    mode: paletteType,
+    background: {
+      default: paletteType === 'light' ? '#eaeaea' : '#121212'
+    }
+  }
+ })
+
+ function handleThemeChange() {
+  setDarkMode(!darkMode);
+ }
+
+  return (
+    <>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+           <Catalog />
+      </Container>
+      </ThemeProvider>
+
+   
+    </>
+  );
+}
+
+export default App;
